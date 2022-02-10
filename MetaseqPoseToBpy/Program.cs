b3af2a01dee5cf66a4ef611e67134e667d089006
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-02-10 22:21:31 +0900

    update: #17 converted part of the project to .NET 5.

--- MetaseqPoseToBpy/Program.cs
+++ MetaseqPoseToBpy/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,10 +7,11 @@ using System.Windows.Forms;
 namespace MetaseqPoseToBpy {
     static class Program {
         /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
