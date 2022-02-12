204e8f88cf8bdc75ec922cd27fe0566c81780155
Hiroyuki Adachi <hiroxpepe@gmail.com>
2022-02-12 19:48:46 +0900

    feat: #19 added the skeleton code.

--- InvertPose/FormMain.Designer.cs
+++ InvertPose/FormMain.Designer.cs
@@ -24,13 +24,42 @@ namespace InvertPose {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.buttonFileDrop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonFileDrop
+            // 
+            this.buttonFileDrop.AllowDrop = true;
+            this.buttonFileDrop.BackColor = System.Drawing.Color.Gray;
+            this.buttonFileDrop.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonFileDrop.Location = new System.Drawing.Point(12, 12);
+            this.buttonFileDrop.Name = "buttonFileDrop";
+            this.buttonFileDrop.Size = new System.Drawing.Size(149, 36);
+            this.buttonFileDrop.TabIndex = 0;
+            this.buttonFileDrop.Text = "ここにドロップ";
+            this.buttonFileDrop.UseVisualStyleBackColor = false;
+            this.buttonFileDrop.DragDrop += new System.Windows.Forms.DragEventHandler(this.buttonFileDrop_DragDrop);
+            this.buttonFileDrop.DragEnter += new System.Windows.Forms.DragEventHandler(this.buttonFileDrop_DragEnter);
+            this.buttonFileDrop.DragOver += new System.Windows.Forms.DragEventHandler(this.buttonFileDrop_DragOver);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(173, 61);
+            this.Controls.Add(this.buttonFileDrop);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "ポーズ反転";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonFileDrop;
     }
 }
 
--- InvertPose/FormMain.cs
+++ InvertPose/FormMain.cs
@@ -1,17 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace InvertPose {
+    /// <summary>
+    /// The main form class.
+    /// </summary>
+    /// <author>Hiroyuki Adachi</author>
     public partial class FormMain : Form {
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        // Constructor
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public FormMain() {
             InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        // Event handler
+
+        /// <summary>
+        /// An event handler where files are dragged and dropped.
+        /// </summary>
+        /// <param name="sender">The event publisher is provided.</param>
+        /// <param name="e">The event parameters are provided.</param>
+        private async void buttonFileDrop_DragDrop(object sender, DragEventArgs e) {
+            if ((e.AllowedEffect & DragDropEffects.Copy) is DragDropEffects.Copy
+                && e.Data.GetDataPresent(DataFormats.FileDrop, true)) {
+                var data = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                if (data is not null) {
+                    foreach (string _filePath in data) {
+                        await Task.Run(() => { });
+                    }
+                    await Task.Run(() => { });
+                }
+            }
+        }
+
+        /// <summary>
+        /// An event handler where files are dragged and entered.
+        /// </summary>
+        /// <param name="sender">The event publisher is provided.</param>
+        /// <param name="e">The event parameters are provided.</param>
+        private void buttonFileDrop_DragEnter(object sender, DragEventArgs e) {
+            if ((e.AllowedEffect & DragDropEffects.Copy) is DragDropEffects.Copy
+                 && e.Data.GetDataPresent(DataFormats.FileDrop)) { // FIXME: to add a validator.
+                e.Effect = DragDropEffects.Copy;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// An event handler where files are dragged and overd.
+        /// </summary>
+        /// <param name="sender">The event publisher is provided.</param>
+        /// <param name="e">The event parameters are provided.</param>
+        private void buttonFileDrop_DragOver(object sender, DragEventArgs e) {
+            if ((e.AllowedEffect & DragDropEffects.Copy) is DragDropEffects.Copy
+                && e.Data.GetDataPresent(DataFormats.FileDrop)) { // FIXME: to add a validator.
+                e.Effect = DragDropEffects.Copy;
+            } else {
+                e.Effect = DragDropEffects.None;
+            }
         }
     }
 }
